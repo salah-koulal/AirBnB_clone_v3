071f7e563e0b5f9e98e70a9c7ec1a8dc34be2b70
mouadnait <moadnait6@gmail.com>
2024-01-09 00:50:21 +0100

    4. Some stats?

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,26 +1,31 @@
 #!/usr/bin/python3
 """This module has route /status"""
 from api.v1.views import app_views
-from flask import jsonify
 from models import storage
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+from models.user import User
 
 
-@app_views.route('/status', methods=["GET"])
+@app_views.route('/status', methods=["GET"], strict_slashes=False)
 def status():
     """return a JSON: 'status': 'OK'"""
-    return jsonify({"status": "OK"})
+    return {
+        "status": "OK",
+    }
 
 
-@app_views.route('/stats', methods=["GET"])
+@app_views.route('/stats', methods=["GET"], strict_slashes=False)
 def stats():
     """Return /status api route"""
-    d = {
-        "amenities": "Amenity",
-        "cities": "City",
-        "places": "Place",
-        "reviews": "Review",
-        "states": "State",
-        "users": "User"
+    return {
+        "amenities": storage.count(Amenity),
+        "cities": storage.count(City),
+        "places": storage.count(Place),
+        "reviews": storage.count(Review),
+        "states": storage.count(State),
+        "users": storage.count(User),
     }
-    d = {k: storage.count(v) for k, v in d.items()}
-    return jsonify(d)
